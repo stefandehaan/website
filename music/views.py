1594233afd83b893631278e3ade9a07a63bf06b9
stefan <stefan@PRODESK13>
2018-11-26 15:01:09 +0100

    added multiple files

--- music/views.py
+++ music/views.py
@@ -17,7 +17,9 @@ def favorite(request, album_id):
     try:
         selected_song = album.song_set.get(pk=request.POST['song'])
     except (KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html', {{'album': album, 'error_message': "you did not select a valid song",}})
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message': "you did not select a valid song"})
     else:
         selected_song.is_favorite = True
         selected_song.save()
